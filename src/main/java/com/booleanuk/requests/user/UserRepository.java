0bbc925fa201375663ea8b1ac571cfdee1b962dd
hanna <hannaphamm1007@gmail.com>
2024-03-19 15:22:41 +0100

    added authentication

--- src/main/java/com/booleanuk/requests/user/UserRepository.java
+++ src/main/java/com/booleanuk/requests/user/UserRepository.java
@@ -1,6 +1,16 @@
 package com.booleanuk.requests.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+    Boolean existsByPhone(String phone);
 }
